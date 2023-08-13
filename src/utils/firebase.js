272be00b1d9340449e9b82199958230c5a4ace57
yogesh <codes.yogesh67@gmail.com>
2023-08-12 20:24:50 -0400

    adding firebase

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -4,14 +4,16 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAHCS05f3YTmnR85ToRT-Ks-xPZMA6djhI",
-    authDomain: "portfolio-19700.firebaseapp.com",
-    databaseURL: "https://portfolio-19700.firebaseio.com",
-    projectId: "portfolio-19700",
-    storageBucket: "portfolio-19700.appspot.com",
-    messagingSenderId: "1049457216523",
-    appId: "1:1049457216523:web:e595ceefc98c7b68dc8b0e",
-    measurementId: "G-RYDJ0VDZ8D",
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+
+
 };
 
 const app = initializeApp(firebaseConfig);
